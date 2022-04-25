e4c4e15c031830ba8af96ff4651dbaade5162f25
Marc <riokost@gmail.com>
2022-04-25 19:50:28 +0200

    Adding some functions to Filtro controller

--- controllers/filtro_controller.js
+++ controllers/filtro_controller.js
@@ -14,4 +14,30 @@ router.get('/', (req, res) => {
         .catch(err => res.status(400).json({ ok: false, data: err }))
 })
 
+//DELETE A Filter
+router.get('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    Filtro.destroy({ where: { id: id } })
+        .then(user => res.status(200).json({ ok: true }))
+        .catch(err => res.status(400).json({ ok: false, data: err }))
+})
+
+//CREATE A Filter
+router.post('/new', function (req, res, next) {
+
+    Filtro.create(req.body)
+        .then(item => res.json({ ok: true, data: item }))
+        .catch((error) => res.json({ ok: false, error }))
+});
+
+//EDIT A Filter
+router.put('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    Filtro.findOne({ where: { id: id } })
+        .then(user => user.update(req.body))
+        .then(() => res.status(200).json({ ok: true }))
+        .catch(err => res.status(400).json({ ok: false, data: err }))
+})
+
+
 module.exports = router;
