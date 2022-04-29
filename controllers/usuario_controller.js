9114fc3bb9bf08aa334e6d0765ec4b7ab1ce6c34
Marc <riokost@gmail.com>
2022-04-29 19:19:47 +0200

    edit user api function

--- controllers/usuario_controller.js
+++ controllers/usuario_controller.js
@@ -5,10 +5,23 @@ const jsonwebtoken = require('jsonwebtoken');
 const Config = require('./config');
 const model = require('../models/index');
 
+const multer = require('multer');
+
 const Usuario = model.Usuario;
 const { secretKey, expiredAfter } = Config;
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage }).single('file');
 
 //FIND ALL USERS
 router.get('/', (req, res) => {
@@ -26,12 +39,45 @@ router.get('/:id', (req, res) => {
 })
 
 //EDIT A USER
-router.put('/edit/:id', (req, res) => {
-    const { id } = req.params;
-    Usuario.findOne({ where: { id: id } })
-        .then(user => user.update(req.body))
-        .then(() => res.status(200).json({ ok: true }))
-        .catch(err => res.status(400).json({ ok: false, data: err }))
+router.put('/edit', (req, res) => {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+        } else if (err) {
+            return res.status(500).json(err)
+        }
+
+        let foto = null;
+        if (!req.file) {
+            // res.status(400).json({ ok: false, error: "dades incomplertes" });
+            foto = req.body.foto;
+        } else {
+            foto = req.file.filename
+        }
+        const usuario = {
+            nombre: req.body.nombre,
+            apellidos: req.body.apellidos,
+            email: req.body.email,
+            nombre_usuario: req.body.nombre_usuario,
+            password: req.body.password,
+            puntuacion: req.body.puntuacion,
+            feed_check: req.body.feed_check,
+            ubicacion_check: req.body.ubicacion_check,
+            activo: req.body.activo,
+            foto_usuario: foto,
+            rol: req.body.rol
+        }
+
+        const { id } = req.body;
+        console.log(usuario, id)
+
+        Usuario.findOne({ where: { id: id } })
+            .then(user => user.update(usuario))
+            .then(() => res.status(200).json({ ok: true }))
+            .catch(err => res.status(400).json({ ok: false, data: err }))
+
+    })
+
 })
 
 //LOGIN
